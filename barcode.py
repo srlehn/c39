1bb4fb8fb4d0cdc2d43ee8420428b1b401b3969b
Simon Lehn <48837958+srlehn@users.noreply.github.com>
2023-09-27 02:30:47 +0200

    add LICENSE (MIT)

--- barcode.py
+++ barcode.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# Copyright 2018 Simon Robin Lehn. All rights reserved.
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file.
 
 ################################################################################
 class barcode:
